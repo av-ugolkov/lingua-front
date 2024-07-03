7e36cd668d9be8f819353750d0dd68f08fe9128c
aleksandr <ugolkov.prog@gmail.com>
2024-07-03 17:48:06 +0300

    get vocabulary name

--- src/pages/Vocabulary.tsx
+++ src/pages/Vocabulary.tsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import SearchAndOrder from '@/components/vocabulary/SearchAndOrder';
 import Words from '@/components/vocabulary/Words';
+import { useGetFetchWithToken } from '@/hooks/fetch/useFetchWithToken';
 
 export default function Vocabulary() {
-  const { name } = useParams<'name'>();
+  const { id } = useParams();
+  const [name, setName] = useState(false);
+
+  const { loading, response } = useGetFetchWithToken(
+    `/account/vocabulary`,
+    new Map([['id', id || '']])
+  );
+
+  useEffect(() => {
+    if (!loading) {
+      if (response.ok) {
+        console.log(response.data);
+        setName(response.data['name']);
+      }
+    }
+  }, [loading, id]);
+
+  if (loading) {
+    return <div></div>;
+  }
 
   return (
     <>
