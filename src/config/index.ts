8bb6b287031e2482a8c3baa0b7c25a0ba41484ba
aleksandr <ugolkov.prog@gmail.com>
2024-07-21 12:30:07 +0300

    remove v0

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,18 +2,16 @@ export interface Config {
   type: string;
   host: string;
   port: number;
-  version: string;
 }
 
 const config: Config = {
   type: 'http',
   host: 'localhost',
   port: 5000,
-  version: 'v0',
 };
 
 export function getAddr(): string {
-  return `${config.type}://${config.host}:${config.port}/${config.version}`;
+  return `${config.type}://${config.host}:${config.port}`;
 }
 
 export default config;
