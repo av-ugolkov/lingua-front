6d46e0cd833680c8d125dc27ffd6f38456bf5ff1
aleksandr <ugolkov.prog@gmail.com>
2024-08-18 12:50:15 +0300

    load pronunciation

--- src/components/vocabulary/InputField.tsx
+++ src/components/vocabulary/InputField.tsx
@@ -2,22 +2,27 @@ export default function InputField({
   value,
   placeholder,
   onChange,
+  children,
 }: {
   value: string;
   placeholder: string;
   onChange: (value: string) => void;
+  children?: React.ReactNode;
 }) {
   return (
-    <input
-      className='flex justify-start bg-transparent w-1/2 border-solid border-[1px] border-black border-t-0 border-x-0 outline-none'
-      type='text'
-      name='word'
-      maxLength={50}
-      placeholder={placeholder}
-      value={value}
-      onChange={(e) => {
-        onChange(e.target.value);
-      }}
-    />
+    <div className='flex justify-start bg-transparent w-1/2 border-solid border-[1px] border-black border-t-0 border-x-0'>
+      <input
+        className='w-full bg-transparent outline-none'
+        type='text'
+        name='word'
+        maxLength={50}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => {
+          onChange(e.target.value);
+        }}
+      />
+      {children}
+    </div>
   );
 }
