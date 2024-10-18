c431c91fa702f38a97b0b3d3b18fc3d9fb7b937d
aleksandr <ugolkov.prog@gmail.com>
2024-10-18 18:16:07 +0300

    fix bugs

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,9 +1,21 @@
+import { useEffect } from 'react';
+
 import SearchInput from '@/components/elements/SearchPanel/SearchInput';
 import SortedPanel from '@/components/elements/SortAndOrder/SortedPanel';
 import { SortUserTypes } from '@/models/Sorted';
 import List from './component/List';
+import { clearVocabs } from '@/redux/vocabularies/slice';
+import { useAppDispatch } from '@/hooks/redux';
 
 export default function Users() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    return () => {
+      dispatch(clearVocabs());
+    };
+  }, [dispatch]);
+
   return (
     <>
       <div className='flex w-full justify-between py-5'>
