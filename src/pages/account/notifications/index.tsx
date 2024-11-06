9c40de8ceabcda3cab6ebd04ccc1f619cad3f41b
aleksandr <ugolkov.prog@gmail.com>
2024-11-06 12:16:37 +0200

    add toggle for show/hide viewed notifications

--- src/pages/account/notifications/index.tsx
+++ src/pages/account/notifications/index.tsx
@@ -1,12 +1,15 @@
 import useFetch from '@/hooks/useFetch.ts';
 import { AuthStore, RequestMethod } from '@/scripts/api.ts';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Event from './component/Event';
 import { IEventData, setEvents } from '@/redux/event/slice';
 import { useAppDispatch, useAppSelector } from '@/hooks/redux';
 
 export default function Notifications() {
   const dispatch = useAppDispatch();
+  const [hideViewEvents, setHideViewEvents] = useState(
+    localStorage.getItem('hideViewEvents') === 'true' ? true : false
+  );
   const { events } = useAppSelector((state) => state.events);
   const { response: respEvents } = useFetch(
     '/events',
@@ -38,8 +41,32 @@ export default function Notifications() {
 
   return (
     <>
-      <div className='flex flex-col mt-5 gap-y-5'>
+      <div className='flex justify-between my-5'>
+        <label
+          htmlFor='events_watched'
+          className='relative inline-flex cursor-pointer items-center'>
+          <input
+            type='checkbox'
+            id='events_watched'
+            className='peer sr-only'
+            checked={hideViewEvents}
+            onChange={(e) => {
+              setHideViewEvents(e.target.checked);
+              localStorage.setItem(
+                'hideViewEvents',
+                e.target.checked.toString()
+              );
+            }}
+          />
+          <div className="h-6 w-11 bg-indigo-200 after:absolute after:top-0.5 after:left-0.5 after:h-5 after:w-5 after:bg-indigo-600 after:shadow after:transition-all after:content-[''] hover:bg-indigo-300 peer-checked:bg-indigo-400 peer-checked:after:translate-x-full peer-disabled:cursor-not-allowed peer-disabled:bg-gray-100 peer-disabled:after:bg-gray-50"></div>
+          <div className='ml-3'>Hide viewed events</div>
+        </label>
+      </div>
+      <div className='flex flex-col my-5 gap-y-5'>
         {events.map((e: IEventData) => {
+          if (hideViewEvents && e.Watched) {
+            return null;
+          }
           return (
             <Event
               key={e.ID}
